1a1165ea07b378163dc91ec9e914d122e556536e
Okiki <abdulkareemrukayat18@gmail.com>
2026-07-29 18:24:20 +0100

    added seo

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,6 +3,6 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://dynah-sweet-treat.vercel.app/sitemap.xml",
+    sitemap: "https://dynah-sweet-treats.vercel.app/sitemap.xml",
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://dynah-sweet-treat.vercel.app",
+      url: "https://dynah-sweet-treats.vercel.app",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
